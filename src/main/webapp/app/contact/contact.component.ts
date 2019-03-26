f4a5f0095022dfa6f76167c19b97d0c550608a1e
henna <henna994@gmail.com>
2019-03-26 14:37:54 -0400

    Added files

---
+++ src/main/webapp/app/contact/contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-contact',
+  templateUrl: './contact.component.html',
+  styles: []
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
